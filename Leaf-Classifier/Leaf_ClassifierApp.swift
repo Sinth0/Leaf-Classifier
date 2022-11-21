5892e445224329c4105002ced0b486d47ecad517
Julian Hoffmeister <julian.hoffmeister3@gmail.com>
2022-11-21 14:15:37 +0100

    Initial Commit

---
+++ Leaf-Classifier/Leaf_ClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leaf_ClassifierApp.swift
+//  Leaf-Classifier
+//
+//  Created by Julian Hoffmeister on 21.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Leaf_ClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
